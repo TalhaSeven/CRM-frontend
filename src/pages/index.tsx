8c9abf9f2dca894c62fd0f7e9d8925f77dd97d9a
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-22 12:28:23 +0300

    Fix formatting issues in next.config.js, postcss.config.js, _app.tsx, _document.tsx, index.tsx, and globals.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <h1 className="text-center text-2xl text-white bg-orange-400 flex justify-center items-center h-40">Hello World</h1>
+      <h1 className="text-center text-2xl text-white bg-orange-400 flex justify-center items-center h-40">
+        Hello World
+      </h1>
     </>
   );
 }
