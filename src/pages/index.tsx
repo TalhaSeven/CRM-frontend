19b14fbf5d100692a37d139d8cd12e9f45fe2667
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-22 10:33:18 +0300

    Update home page with "Hello World" heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center text-2xl text-white bg-orange-400 flex justify-center items-center h-40">Hello World</h1>
+    </>
+  );
 }
